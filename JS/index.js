2d9b506ea4e66c26370df335c14c5f7756dd30e6
JulianCenfotec <jcabreram@ucenfotec.ac.cr>
2024-06-14 16:48:41 -0600

    Eliminar Iframe y separacion del JS

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+import 'https://code.jquery.com/jquery-3.6.0.min.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js';
+import 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Document loaded");
+});
