0a3584c67f79b0c03096daeb9a1cad571417d0c8
JulianCenfotec <jcabreram@ucenfotec.ac.cr>
2024-06-23 19:49:18 -0600

    eliminacion del jquery

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,3 @@
-import 'https://code.jquery.com/jquery-3.6.0.min.js';
 import 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js';
 import 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
 
