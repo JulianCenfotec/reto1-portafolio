49c7caa7e6d73af439ade9d500d7434a72d17f7f
JulianCenfotec <jcabreram@ucenfotec.ac.cr>
2024-06-23 19:53:26 -0600

    eliminacion del jquery

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,5 @@
 import 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js';
-import 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
-
+//se elimina la importación de jquery y bootstrap, ya que no se están utilizando en el proyecto
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Document loaded");
 });
